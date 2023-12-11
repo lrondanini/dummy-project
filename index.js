0787c43efb8fa57d8db78bb8806eb4121982d0ab
tobia <luca.rondanini@gmail.com>
2023-12-10 18:08:13 -0800

    simple server

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from 'http';
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("<html><body><div>Dummy Project</div></body></html>");
+};
+
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
